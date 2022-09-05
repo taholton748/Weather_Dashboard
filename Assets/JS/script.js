5ce1a2d85474b23df15c5174f6c5f7e7dae46f47
Medici <107539009+taholton748@users.noreply.github.com>
2022-09-05 17:53:15 -0600

    added API links and fetch requests

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,3 +9,32 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+var locationUrl =
+  "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=627e2e424592ad588e61a158f86366c9";
+var weatherUrl =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=627e2e424592ad588e61a158f86366c9";
+
+var dataDisplay = document.getElementById("cityData");
+
+fetch(locationUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var locationArray = data.response.docs;
+    for (var i = 0; i < locationArray.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = locationArray[i].dataDisplay //something;
+        .appendChild(listItem);
+    }
+  });
+
+fetch(weatherUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
